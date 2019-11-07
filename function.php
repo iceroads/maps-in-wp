fdfdc4f9a203607d4d8b22c67cfde31fc7f13da1
Pontus Nilsson <iceroads@gmail.com>
2019-11-07 16:18:24 +0100

    removed the api key

--- function.php
+++ function.php
@@ -4,7 +4,7 @@
  */
 function googleMapsFunction() {
     // Google maps init function
-    wp_enqueue_script( 'googleMaps','https://maps.googleapis.com/maps/api/js?key=AIzaSyDwLBvtId4nbZuw1RUgMTJ82-GjGKaJZGE&callback=initMap', array('googleMapsInit'), '1.0', true );
+    wp_enqueue_script( 'googleMaps','https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap', array('googleMapsInit'), '1.0', true );
     wp_enqueue_script( 'googleMapsInit', get_theme_file_uri( '/js/map.js' ), array(), '1.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'googleMapsFunction' );
