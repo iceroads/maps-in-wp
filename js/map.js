fe80bf84851fccac49fe9891f93dcb6be332112f
Pontus Nilsson <iceroads@gmail.com>
2019-11-07 16:41:07 +0100

    added marker instead of InfoWindow

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 // Funktion som kallas av Google för att skapa vår karta
 // Denna function anger vi i en callback parameter i script
-var map, infoWindow;
+var map;
 function initMap() {
     // Sätt latitude och longitud i en variabel
     var uluru = {lat: -25.344, lng: 131.036};
@@ -13,21 +13,6 @@ function initMap() {
             gestureHandling: 'none'
         }
     );
-    // Information ruta
-    var contentString = '<div id="content">\n' +
-        '    <div id="siteNotice"></div>\n' +
-        '    <h1 id="firstHeading" class="firstHeading">Google Maps API är skoj :)</h1>\n' +
-        '    <div id="bodyContent">\n' +
-        '        <p>\n' +
-        '            <b>Hej allihopa!</b>\n' +
-        '            Här har vi nu en text som visas i en ruta när man klickar på markören :)<br>\n' +
-        '            Super bra om man vill visa information om något!\n' +
-        '        </p>\n' +
-        '    </div>\n' +
-        '</div>';
-
-    // Instansiera en nya information ruta och sätt ovanstående html till den
-    infoWindow = new google.maps.InfoWindow;
 
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
@@ -37,9 +22,15 @@ function initMap() {
                 lng: position.coords.longitude
             };
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent(contentString);
-            infoWindow.open(map);
+            // Sätt ut en markering på kartan med positionen från vår variabel
+            new google.maps.Marker(
+                {
+                    position: pos,
+                    map: map,
+                    icon: 'https://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png'
+                }
+            );
+
             map.setCenter(pos);
             map.setZoom(15);
         }, function() {
@@ -49,20 +40,6 @@ function initMap() {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
-
-    // Sätt ut en markering på kartan med positionen från vår variabel
-    var marker = new google.maps.Marker(
-        {
-            position: uluru,
-            map: map,
-            icon: 'https://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png'
-        }
-    );
-    // Lägg till en listener på markören.
-    // Om vi klickar ska vi öppna informationsrutan och placera den på samma ställe som markören
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-    });
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
